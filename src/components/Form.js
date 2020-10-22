4710ccc10c9456dce64f1b65f0affe230f4b265e
Phil9219 <philipp.fell@yahoo.de>
2020-10-22 22:08:14 +0200

    Fetch API-Data

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,23 @@
 import { createElement } from "../utils/elements";
 import "./form.css";
 
-export const createForm = () => {
-  const input = createElement("input", {
-    className: "input",
-    placeholder: "ENTER YOUR LOCATION",
-  });
+import { getWeather, createWeatherElements } from "../utils/api";
 
+export const createForm = () => {
   const searchButton = createElement("button", {
     className: "searchButton",
     innerText: "☀️",
+
+    onclick: async (event) => {
+      event.preventDefault();
+      let weatherObj = getWeather(input.value);
+      createWeatherElements(weatherObj);
+    },
+  });
+
+  const input = createElement("input", {
+    className: "input",
+    placeholder: "ENTER YOUR LOCATION",
   });
 
   const form = createElement("form", {
